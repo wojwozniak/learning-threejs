cd4a79c1d27e04513b3f83136f7b23bac62bc8a0
pulchnymamut <pulchnymamut@gmail.com>
2022-08-31 20:43:22 +0200

    checkpoint

--- 3js-tutorial/main.js
+++ 3js-tutorial/main.js
@@ -1,5 +1,47 @@
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, .1, 1000);
+const renderer = new THREE.WebGLRenderer();
 
-console.log(scene);
+renderer.setSize(innerWidth, innerHeight);
+renderer.setPixelRatio(devicePixelRatio);
+document.body.appendChild(renderer.domElement);
+
+/*
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00FF00 });
+
+
+const mesh = new THREE.Mesh(boxGeometry, material);
+
+scene.add(mesh);
+
+*/
+
+camera.position.z = 5;
+
+const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10);
+const planeMaterial = new THREE.MeshPhongMaterial({
+  color: 0xFF0000,
+  side: THREE.DoubleSide
+});
+
+const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+scene.add(planeMesh);
+
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(0, 0, 1);
+scene.add(light);
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+  /*
+  mesh.rotation.x += .01;
+  mesh.rotation.y += .01;
+  */
+  //planeMesh.rotation.x += .02;
+}
+
+animate();
