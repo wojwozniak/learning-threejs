26879f8781f7a50bc5ea06414b3d1b67e5a3a328
pulchnymamut <pulchnymamut@gmail.com>
2022-09-03 21:17:22 +0200

    update

--- src/components/earth/Earth.jsx
+++ src/components/earth/Earth.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useLoader } from '@react-three/fiber';
+import React, {useRef} from 'react';
+import { useLoader, useFrame } from '@react-three/fiber';
 import { OrbitControls, Stars } from '@react-three/drei';
 import * as THREE from 'three';
 
@@ -12,19 +12,29 @@ import { TextureLoader } from 'three';
 
 export function Earth(props) {
 
-    const [colorMap, normalMap, specularMap, cloudsMap] = useLoader(TextureLoader, [EarthDayMap, EarthNormalMap, EarthSpecularMap, EarthCloudsMap]);
+  const [colorMap, normalMap, specularMap, cloudsMap] = useLoader(TextureLoader, [EarthDayMap, EarthNormalMap, EarthSpecularMap, EarthCloudsMap]);
+  
+  const earthRef = useRef();
+  const cloudsRef = useRef();
+
+  useFrame(({ clock }) => {
+    const elapsedTime = clock.getElapsedTime();
+    earthRef.current.rotation.y = elapsedTime / 24;
+    cloudsRef.current.rotation.y = elapsedTime / 18;
+  });
 
     return <>
-      <ambientLight intensity={1} />
-      <Stars />
-        <mesh>
+      {/*<ambientLight intensity={.8} /> */}
+      <pointLight color="#f6f3ea" position={[2, 0, 2]} intensity={ 1.2 } />
+      <Stars radius={300} depth={60} count={20000} factor={7} saturation={ .01 } fade={true} />
+        <mesh ref={cloudsRef}>
             <sphereGeometry args={[1.005, 32, 32]} />
             <meshPhongMaterial map={cloudsMap} opacity={.4} deepWrite={true} transparent={true} side={THREE.DoubleSide} />
         </mesh>
-        <mesh>
+        <mesh ref={earthRef}>
             <sphereGeometry args={[1, 32, 32]} />
             <meshPhongMaterial specularMap={specularMap} />
-            <meshStandardMaterial map={colorMap} normalMap={normalMap} />
+            <meshStandardMaterial map={colorMap} normalMap={normalMap} metalness={.4} roughness={.7} />
             <OrbitControls enableZoom={true} enablePan={true} enableRotate={true} zoomSpeed={.6} panSpeed={.5} rotateSpeed={.4} />
         </mesh>
     </>;
